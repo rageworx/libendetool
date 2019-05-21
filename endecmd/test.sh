de0e57acf6d135b8c560b7a5cb2e78a8fa7a9462
Raphael Kim <rageworx@gmail.com>
2019-05-21 05:33:33 +0000

    test.sh updated for Linux and MacOSX.

--- endecmd/test.sh
+++ endecmd/test.sh
@@ -2,8 +2,8 @@
 KEY="12345678901234567890123456789012"
 SRC="Testing_source_words_for_libendetool_and_endecmd."
 ENDECMD=bin/endecmd
-
-TESTRUN=$(shell ${ENDECMD} --help )
+#TESTRUN=$(shell ${ENDECMD} --help)
+TESTRUN=$(${ENDECMD} --help)
 
 # test is it compiled.
 if [ "${TESTRUN}" == "" ];then
@@ -12,10 +12,15 @@ if [ "${TESTRUN}" == "" ];then
     exit 0
 fi
 
-ENCDATA=$(shell ${ENDECMD} --key=${KEY} --verboseoff ${SRC})
-DECDATA=$(shell bin/endecmd --key=${KEY} --decode --verboseoff ${ENCDATA})
-COMENCDATA=$(shell ${ENDECMD} --key=${KEY} --verboseoff --compress ${SRC})
-COMDECDATA=$(shell ${ENDECMD} --key=${KEY} --verboseoff --decode --compress ${COMENCDATA})
+#ENCDATA=$(shell ${ENDECMD} --key=${KEY} --verboseoff ${SRC})
+#DECDATA=$(shell bin/endecmd --key=${KEY} --decode --verboseoff ${ENCDATA})
+#COMENCDATA=$(shell ${ENDECMD} --key=${KEY} --verboseoff --compress ${SRC})
+#COMDECDATA=$(shell ${ENDECMD} --key=${KEY} --verboseoff --decode --compress ${COMENCDATA})
+ENCDATA=$(${ENDECMD} --key=${KEY} --verboseoff ${SRC})
+DECDATA=$(${ENDECMD} --key=${KEY} --decode --verboseoff ${ENCDATA})
+COMENCDATA=$(${ENDECMD} --key=${KEY} --verboseoff --compress ${SRC})
+COMDECDATA=$(${ENDECMD} --key=${KEY} --verboseoff --decode --compress ${COMENCDATA})
+
 
 echo "source : ${SRC}"
 echo "key : ${KEY}"
